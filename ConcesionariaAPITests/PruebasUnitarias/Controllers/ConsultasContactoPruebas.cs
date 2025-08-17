682bb7163f00d273424f53426ff2e9d687ce9581
elflacoseba <sebaserri@gmail.com>
2025-08-17 14:40:15 -0300

    Creada prueba unitaria para el Post de crear ConsultaContacto.

--- ConcesionariaAPITests/PruebasUnitarias/Controllers/ConsultasContactoPruebas.cs
+++ ConcesionariaAPITests/PruebasUnitarias/Controllers/ConsultasContactoPruebas.cs
@@ -2,11 +2,13 @@ using Concesionaria.API.Controllers;
 using Concesionaria.Application.DTOs;
 using Concesionaria.Application.Interfaces;
 using Concesionaria.Application.Services;
+using Concesionaria.Application.Validators;
 using Concesionaria.Domain.Entities;
 using Concesionaria.Infrastructure.Repositories;
 using ConcesionariaAPITests.Utilidades;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConcesionariaAPITests;
 
@@ -19,7 +21,6 @@ public class ConsultasContactoPruebas : BasePruebas
         //Preparación
         var nombreBD = Guid.NewGuid().ToString();
         var context = ConstruirContext(nombreBD);
-        //var mapper = ConfigurarMapper();
         IGenericRepository<ConsultaContacto> repository = new GenericRepository<ConsultaContacto>(context);
         IValidator<ConsultaContactoCreacionDTO> validatorConsultaCreacionDTO = null!;
         IValidator<ConsultaContactoActualizacionDTO> validatorConsultaActualizacionDTO = null!;
@@ -43,7 +44,6 @@ public class ConsultasContactoPruebas : BasePruebas
 
         var nombreBD = Guid.NewGuid().ToString();
         var context = ConstruirContext(nombreBD);
-      //  var mapper = ConfigurarMapper();
         var context2 = ConstruirContext(nombreBD);
         IGenericRepository<ConsultaContacto> repository = new GenericRepository<ConsultaContacto>(context2);
         IValidator<ConsultaContactoCreacionDTO> validatorConsultaCreacionDTO = null!;
@@ -85,5 +85,40 @@ public class ConsultasContactoPruebas : BasePruebas
         var resultado = respuesta.Value;
 
         Assert.AreEqual(expected: 1, actual: resultado!.Id,"Se espera obtener una consulta contacto con el Id = 1.");
+    }
+
+    [TestMethod]
+    public async Task Post_DebeCrearConsultaContacto()
+    {
+        //Preparación
+        var nombreBD = Guid.NewGuid().ToString();
+        var context = ConstruirContext(nombreBD);
+        var context2 = ConstruirContext(nombreBD);
+        IGenericRepository<ConsultaContacto> repository = new GenericRepository<ConsultaContacto>(context2);
+        IValidator<ConsultaContactoCreacionDTO> validatorConsultaCreacionDTO = new ConsultaContactoCreacionDTOValidator();
+        IValidator<ConsultaContactoActualizacionDTO> validatorConsultaActualizacionDTO = null!;
+        var consultaContactoService = new ConsultaContactoService(repository, validatorConsultaCreacionDTO, validatorConsultaActualizacionDTO);
+
+        var consultaContactoCreacionDTO = new ConsultaContactoCreacionDTO
+        {
+            Nombre = "Nombre1",
+            Email = "miemail@gmail.com",
+            Telefono = "123456789",
+            Mensaje = "Mensaje de prueba"
+        };
+
+        var controller = new ConsultasContactoController(consultaContactoService);
+
+        //Prueba
+        var respuesta = await controller.Post(consultaContactoCreacionDTO);
+
+        //Verificación
+        var resultado = respuesta as CreatedAtRouteResult;
+
+        Assert.IsNotNull(resultado, "Se esperaba un resultado de tipo CreatedAtRouteResult.");
+
+        var cantidad = await context2.ConsultasContacto.CountAsync();
+
+        Assert.AreEqual(expected: 1, actual:cantidad, "Se espera obtener una consulta contacto.");
     }
 }
