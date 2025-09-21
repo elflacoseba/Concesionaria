33b3471832d870bcc020f7b3e5cdfb27c1615a7c
Sebastián Serrisuela <sebaserri@gmail.com>
2025-09-20 21:29:42 -0300

    Agrego Identity en la API para manejar Usuarios y Roles.

---
+++ Concesionaria.API/DTOs/UsuarioCrearDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concesionaria.API.DTOs
+{
+    public record UsuarioCrearDto
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage = "El nombre de usuario es obligatorio.")]
+        public string UserName { get; set; } = string.Empty;
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El email no es válido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La contraseña es obligatoria.")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
